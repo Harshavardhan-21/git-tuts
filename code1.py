e68039dd350c272a852e6c16bcc340401e62610c
BHV <harshavardhan95733@gmail.com>
2025-01-08 16:47:51 +0530

    Commiting code & text file

---
+++ code1.py
@@ -0,0 +1,49 @@
+HTML-- HyperText Markup Language
+HyperText--link between webpages
+Markup Language-- Text between tags which defines structure.
+->language to create the web pages.
+how the web page looks and how to displat content with the help of elements 
+It forms or defines the structure of our webpages.
+need to save by the ".html" extension.
+
+-->HTML Features
+ -learning curve is very easy(easy to modify)
+ -create the effective presentations.
+ -add links wherein we can add references.
+ -can display docs on platforms like MAC, WINDOWS, LINUX etc.
+ - Add  videos, grafics and audios making it more attractive.
+ -It is the case insensitive language.
+ 
+-->HTML Editors
+ -Notepad(simple)
+ -Notepad++
+ -Atom
+ -Sublime Text(Best)
+
+--> HTML Skeleton
+ -"<!DOCTYPE HTML>" --Instruction to browser about the Html version
+ -"<HTML>" --Root element acts as container to hold all codes.
+		   --Browser should know this a HTML docs
+		   --Permitted content: One head tag followed by one body tag.
+ -"<HEAD>" --Everything written here will never be displayed in browser
+		   --Contains general info about docs
+				--Title,definations of css and scriptsheets
+				--Metadata(info about the docs)
+ -"<BODY>" --Everythingwritten here will be displayed in browser
+		   --Contains Text, images, links which can be achieved through tags.
+		   --like:-
+				<p> this is our paragraph. </p>
+				<a href="www.google.com">Go to Google</a>
+				<img src ="photo.jpg">
+===========
+	<!DOCTYPE HTML>
+	<HTML>
+	<HEAD>
+			<title>
+			</title>
+	</HEAD>
+		<BODY>
+	
+		</BODY>
+	</HTML>
+==========
